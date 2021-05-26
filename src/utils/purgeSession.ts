ced64cf015ab076d83bbf8bcefa80061fea477c8
sjtommylee <sjtommylee@gmail.com>
2021-05-25 22:44:39 -0700

    0525

--- src/utils/purgeSession.ts
+++ src/utils/purgeSession.ts
@@ -1,10 +1,13 @@
-// import {Redis} from "ioredis";
-// import { redis } from "redis";
-// /**
-//  * @method lrange - LRANGE will return specified elements of the list stored at the key.
-//  */
-// export const purgeSession = async (userId, redis) => {
-//     const sessionIds = await redis.lrange(
-//         `${}${userId}`
-//     )
-// }
+import { Redis } from "ioredis";
+import { userPrefix, redisPrefix } from "../constants";
+
+// purge the session, clear all user sessions from redis cache
+export const purgeSession = async (userId: string, redis: Redis) => {
+  const sessionIds = await redis.lrange(`${userPrefix}${userId}`, 0, -1);
+  const promises = [];
+  for (let i = 0; i < sessionIds.length; i += 1) {
+    promises.push(redis.del(`${redisPrefix}${sessionIds[i]}`));
+  }
+
+  await Promise.all(promises);
+};
